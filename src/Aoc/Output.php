4860ac3e26058466d995b87a597c19a25385dff0
Maciej Łebkowski <m.lebkowski@gmail.com>
2023-06-16 20:32:30 +0200

    don’t use erase line in output, overwrite using spaces instead

--- src/Aoc/Output.php
+++ src/Aoc/Output.php
@@ -6,18 +6,22 @@ namespace App\Aoc;
 
 final class Output
 {
+    private static $lastOutputLength = 0;
+
     public static function step(mixed $value, string $meta = ''): void
     {
         $separator = $meta ? ' ' : '';
+        $lastOutputLength = self::$lastOutputLength;
         $len = strlen((string)$value) + strlen($meta) + strlen($separator);
-        $eraseUntilEndOfLine = "\033[K";
+        self::$lastOutputLength = $len;
+        $padding = str_pad(' ', max(0, $lastOutputLength - $len));
+        $len += strlen($padding);
         $moveCursorLeft = "\033[{$len}D";
         $yellowBackground = "\033[0;43m";
         $greenText = "\033[32m";
         $restoreColor = "\033[0m";
 
         echo implode("", [
-            $eraseUntilEndOfLine,
             $yellowBackground,
             $meta,
             $restoreColor,
@@ -25,6 +29,7 @@ final class Output
             $greenText,
             $value,
             $restoreColor,
+            $padding,
             $moveCursorLeft,
         ]);
     }
