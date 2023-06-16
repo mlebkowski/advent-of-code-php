3b874bd47dff08faa551cc64fbe1b6554f7357ad
Maciej Łebkowski <m.lebkowski@gmail.com>
2023-06-16 17:46:19 +0200

    progress of unknown iterations

--- src/Aoc/Progress.php
+++ src/Aoc/Progress.php
@@ -17,15 +17,26 @@ final class Progress
 
     public static function ofExpectedIterations(int $expectedIterations): self
     {
-        assert($expectedIterations > 0);
         $start = microtime(true);
         return new self($expectedIterations, $start);
     }
 
-    public function __construct(
-        private readonly int $expectedIterations,
+    public static function unknown(): self
+    {
+        $start = microtime(true);
+        return new self(null, $start);
+    }
+
+    private function __construct(
+        private readonly null|int $expectedIterations,
         private readonly float $start,
     ) {
+        if (null === $this->expectedIterations) {
+            $this->iterationPadding = 0;
+            return;
+        }
+
+        assert($expectedIterations > 0);
         $iterationPadding = (int)floor(log10($expectedIterations)) + 1;
         $iterationPadding += floor(($iterationPadding - 1) / 3);
         $this->iterationPadding = (int)$iterationPadding;
@@ -42,17 +53,30 @@ final class Progress
 
     public function report(mixed $value): true
     {
-        $percentage = $this->iteration / $this->expectedIterations;
         $elapsed = microtime(true) - $this->start;
+        $iteration = str_pad(
+            number_format($this->iteration, thousands_separator: ' '),
+            $this->iterationPadding,
+            pad_type: STR_PAD_LEFT,
+        );
+
+        if (null === $this->expectedIterations) {
+            $meta = sprintf(
+                '[ %s / %ds ]',
+                $iteration,
+                $elapsed,
+            );
+
+            Output::step($value, $meta);
+            return true;
+        }
+
+        $percentage = $this->iteration / $this->expectedIterations;
         $expectedTime = (round($elapsed) / $percentage) * (1 - $percentage);
 
         $meta = sprintf(
             '[ %s / %s / %s ]',
-            str_pad(
-                number_format($this->iteration, thousands_separator: ' '),
-                $this->iterationPadding,
-                pad_type: STR_PAD_LEFT,
-            ),
+            $iteration,
             $percentage > 1 ? '??%' : sprintf('% 2.0f%%', $percentage * 100),
             $percentage > 1 ? '??s' : sprintf('%ds', $expectedTime),
         );
