6aad8d76e7d2af899ad678b45ed753b23aded785
Maciej Łebkowski <m.lebkowski@gmail.com>
2023-07-05 23:58:42 +0200

    2017/11: part II - stupid brute-force

--- src/Solutions/Y2017/D11/HexEd.php
+++ src/Solutions/Y2017/D11/HexEd.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace App\Solutions\Y2017\D11;
 
 use App\Aoc\Challenge;
+use App\Aoc\Progress\Progress;
 use App\Aoc\Runner\RunMode;
 use App\Aoc\Solution;
+use loophp\collection\Collection;
 
 /**
  * @implements Solution<HexEdInput>
@@ -25,6 +27,16 @@ final class HexEd implements Solution
 
     public function solve(Challenge $challenge, mixed $input, RunMode $runMode): mixed
     {
-        return StepCounter::count(...$input->directions);
+        if ($challenge->isPartOne()) {
+            return StepCounter::count(...$input->directions);
+        }
+
+        $progress = Progress::ofExpectedIterations(count($input->directions));
+        return Collection::fromIterable($input->directions)
+            ->window(-1)
+            ->apply($progress->step(...))
+            ->map(static fn (array $directions) => StepCounter::count(...$directions))
+            ->apply($progress->report(...))
+            ->max();
     }
 }
