392df3f52e33e931b3e8252e29b305387457a8ee
Maciej Łebkowski <m.lebkowski@gmail.com>
2023-07-14 09:39:37 +0200

    Lib/Filters

---
+++ src/Lib/Filters/Number.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Lib\Filters;
+
+use Closure;
+
+final readonly class Number
+{
+    public static function greaterThan(int $value): Closure
+    {
+        return static fn (int $other) => $other > $value;
+    }
+
+    public static function greaterThanOrEqual(int $value): Closure
+    {
+        return static fn (int $other) => $other >= $value;
+    }
+}
